49918e28f9e6b4b6ea315a8f89bddf3ef69cff9d
Trisha-Ray <trisha.ray@stu.ocsb.ca>
2025-04-21 16:01:28 +0000

    answer php and index done

--- answer.php
+++ answer.php
@@ -19,36 +19,37 @@
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script src="./js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Random Number Guess, in Javascript</span>
+        <span class="mdl-layout-title">Random Number Guess, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/dice.gif" alt="dice gif" />
+        <img src="./images/dice.gif" />
       </div>
-      <div class="page-content">Pick a number between 1 and 6</div>
-      <br />
-      <form>
-        <br />
-        <div class="page-content-php">
-          <form action="./answer.php" method="GET">
-            <div class="mdl-textfield mdl-js-textfield">
-              <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="userNumber" />
-              <label class="mdl-textfield__label" for="userNumber">enter</label>
-              <span class="mdl-textfield__error">Input is not a number!</span>
-            </div>
-            <br />
-            <!-- Accent-colored raised button with ripple -->
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-              type="submit">
-              Check!
-            </button>
-          </form>
+      <div class="page-content-php">
+        <div id="check">
+          <?php
+          $randomNumber = rand(1, 6);
+          //input
+          $userNumber = $_GET["number"];
+          // process
+          $userNumber = (int) $_GET["userNumber"];
+          $randomNumber = rand(1, 6);
+
+          if ($userNumber === $randomNumber) {
+            echo "You have guessed the correct number!";
+          } else {
+            echo "You have guessed the wrong number!<br/>The Correct Number Was: " . $randomNumber;
+          }
+          ?>
         </div>
+        <div class="page-content-answer">
+          <a href="./index.php">Return ...</a>
+        </div>
+      </div>
     </main>
   </div>
 </body>
